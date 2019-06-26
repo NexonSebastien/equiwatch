879d4c1a3bbd4f640f9e34fe78de7b3c73a8b19c
NolanGougeon <gnolan@hotmail.fr>
2019-06-26 22:19:57 +0200

    [Temp] Mise en place d'un bouton pour ouvrir la map

--- app/src/main/java/fr/equiwatch/MainActivity.java
+++ app/src/main/java/fr/equiwatch/MainActivity.java
@@ -1,14 +1,31 @@
 package fr.equiwatch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt_maps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_maps = findViewById(R.id.bt_maps);
+        bt_maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMaps();
+            }
+        });
+    }
+
+    public void openMaps() {
+        Intent intent = new Intent(this, MapsEquiwatch.class);
+        startActivity(intent);
     }
 }
