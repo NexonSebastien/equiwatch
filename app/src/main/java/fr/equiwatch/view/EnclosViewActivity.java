25876faab981986c4123ed14aae2f2c17ac28343
NolanGougeon <gnolan@hotmail.fr>
2019-07-15 16:19:25 +0200

    Affichage de EnclosViewActivity au clic d'un item de la list

--- app/src/main/java/fr/equiwatch/view/EnclosViewActivity.java
+++ app/src/main/java/fr/equiwatch/view/EnclosViewActivity.java
@@ -1,10 +1,16 @@
 package fr.equiwatch.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import fr.equiwatch.R;
+import fr.equiwatch.model.EnclosClass;
 
 public class EnclosViewActivity extends MenuEquiwatch  {
 
@@ -14,6 +20,13 @@ public class EnclosViewActivity extends MenuEquiwatch  {
         CoordinatorLayout dynamicContent = findViewById(R.id.dynamic_content);
         getLayoutInflater().inflate(R.layout.activity_enclos_view, dynamicContent, true);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enclos_view);
+
+        Intent intent = getIntent();
+
+        if(intent != null && intent.getExtras() != null) {
+            EnclosClass enclos =(EnclosClass) intent.getSerializableExtra("enclos");
+            TextView dynamicLabel = findViewById(R.id.dynamycLabel);
+            dynamicLabel.setText(enclos.getLabel());
+        }
     }
 }
