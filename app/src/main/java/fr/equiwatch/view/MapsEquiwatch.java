8a6944d1d146dcb242914a4d73b7efa02ff61c2b
NolanGougeon <gnolan@hotmail.fr>
2019-07-16 16:30:11 +0200

    On affiche les noms de rues

--- app/src/main/java/fr/equiwatch/view/MapsEquiwatch.java
+++ app/src/main/java/fr/equiwatch/view/MapsEquiwatch.java
@@ -85,7 +85,7 @@ public class MapsEquiwatch extends SupportMapFragment implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         mMap.getUiSettings().setMapToolbarEnabled(false);
 
